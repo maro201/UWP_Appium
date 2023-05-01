991180051029d1aa1d066d61a3a56f8e245e3ada
marius.secu <marius.secu@zitec.com>
2023-05-01 21:29:11 +0300

    add entitycore

--- myApp/Models/MyAppDbContext.cs
+++ myApp/Models/MyAppDbContext.cs
@@ -1,12 +1,17 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.UI;
 
 namespace myApp.Models
 {
-    internal class MyAppDbContext
+    public class MyAppDbContext : DbContext
     {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
     }
 }
